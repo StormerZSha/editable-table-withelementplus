9831c07b28ceaaed387ad23e7763b78cb4f28196
StormerZSha <578057966@qq.com>
2023-06-27 15:06:11 +0800

    add add&remove function

--- src/components/editableTable.vue
+++ src/components/editableTable.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="editableTable">
         <div class="tableTop">
-            <el-button  type="primary" >Add</el-button>
+            <el-button type="primary" @click="addData">Add</el-button>
         </div>
         <div class="tableContainer">
             <el-table
@@ -19,7 +19,7 @@
                 <el-table-column fixed="right" label="Operations" width="120">
                     <template #default>
                       <el-button link type="primary" size="small">Edit</el-button>
-                      <el-button link type="danger" size="small">Remove</el-button>
+                      <el-button link type="danger" size="small" @click="removeData">Remove</el-button>
                     </template>
                 </el-table-column>
             </el-table>
@@ -27,7 +27,9 @@
     </div>
 </template>
 <script setup>
+import { ElMessage } from 'element-plus';
 import {ref} from 'vue'
+
 let tableData=ref([
    {
     name: 'Tom',
@@ -59,6 +61,30 @@ let tableData=ref([
   }   
 ])
 let tableKey=ref(0)
+let editingIndex=ref(null)
+
+const addData=()=>{
+    if (editingIndex.value!=null) {
+        ElMessage.warning('please finish editing ! ')
+        return;
+    }
+    tableData.value.push({
+        name: 'Tom',
+        state: 'California',
+        city: 'Los Angeles',
+        address: 'No. 189, Grove St, Los Angeles',
+        zip: 'CA 90036',
+    })
+    tableKey.value++
+}
+const removeData=(index)=>{
+    if (editingIndex.value!=null) {
+        ElMessage.warning('please finish editing ! ')
+        return;
+    }
+    tableData.value.splice(index,1)
+    tableKey.value++
+}
 </script>
 <style lang="scss">
 .editableTable{
