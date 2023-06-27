52a2ac6311fe67552ca82f0b64809654b6590653
StormerZSha <578057966@qq.com>
2023-06-27 14:27:58 +0800

    complete dom

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-
+    <editableTable />
   </div>
 </template>
 <script setup>
-
+import editableTable from './components/editableTable.vue';
 </script>
 <style scoped>
 </style>
---
+++ src/components/editableTable.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="editableTable">
+        <div class="tableTop">
+            <div class="addbtn">Add</div>
+        </div>
+        <div class="tableContainer">
+            <el-table
+                ref="table"
+                :key="tableKey"
+                :data="tableData"
+                border
+                fit
+            >
+                <el-table-column prop="name" label="Name" width="120" />
+                <el-table-column prop="state" label="State" width="120" />
+                <el-table-column prop="city" label="City" width="120" />
+                <el-table-column prop="address" label="Address" width="600" />
+                <el-table-column prop="zip" label="Zip" width="120" />
+                <el-table-column fixed="right" label="Operations" width="120">
+                    <template #default>
+                      <el-button link type="primary" size="small">Edit</el-button>
+                      <el-button link type="danger" size="small">Remove</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </div>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style>
+
+</style>
